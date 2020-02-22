43838ebe5ddaef8f1230e9b82cba6f420eaa9baa
skyrunwith <582547045@qq.com>
2020-02-22 14:07:33 +0800

    Eureka Server安全认证

---
+++ microservice-discovery-eureka-authenticating/src/main/java/com/fzd/cloud/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.fzd.cloud;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 功能描述:
+ *
+ * @author: FZD
+ * @date: 2020/2/21
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
