65cd09265bf895c09d1ad93d847d9878d730b791
付祖德 <skyrunwith>
2020-02-23 17:27:59 +0800

    Feign使用Hystrix - FallbackFactory获得造成回退的原因 FeignHystrix 局部启用、局部禁用

---
+++ microservice-consumer-movie-feign-hystrix-fallback-factory/src/main/java/com/fzd/cloud/UserFeignClient.java
@@ -0,0 +1,55 @@
+package com.fzd.cloud;
+
+import feign.Feign;
+import feign.hystrix.FallbackFactory;
+import feign.hystrix.HystrixFeign;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "microservice-provider-user", fallbackFactory = UserFeignClientFallbackFactory.class
+        , configuration = FeignEnableHystrixConfiguration.class)
+public interface UserFeignClient {
+    @GetMapping("/users/{id}")
+    User getUser(@PathVariable Long id);
+}
+
+//获得造成fallback的原因
+@Component
+@Slf4j
+class UserFeignClientFallbackFactory implements FallbackFactory<UserFeignClient> {
+
+    @Override
+    public UserFeignClient create(Throwable cause) {
+        return new UserFeignClient() {
+            @Override
+            public User getUser(Long id) {
+                log.error("进入回退方法", cause);
+                return new User(id, "默认用户", "默认用户", 0, new BigDecimal(0));
+            }
+        };
+    }
+}
+
+//局部禁用
+class FeignEnableHystrixConfiguration {
+    @Bean
+    @Scope(value = "prototype")
+    public HystrixFeign.Builder builder() {
+        return new HystrixFeign.Builder();
+    }
+}
+//局部启用
+class FeignDisableHystrixConfiguration {
+    @Bean
+    @Scope("prototype")
+    public Feign.Builder feignBuilder() {
+        return Feign.builder();
+    }
+}
