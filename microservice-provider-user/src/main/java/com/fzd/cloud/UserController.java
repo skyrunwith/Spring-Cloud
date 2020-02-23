ff61ae9085cca9e506c74d4bc6850645b322b7ac
付祖德 <skyrunwith>
2020-02-23 16:36:11 +0800

    Eureka服务注册与发现-服务提供者

--- microservice-provider-user/src/main/java/com/fzd/cloud/UserController.java
+++ microservice-provider-user/src/main/java/com/fzd/cloud/UserController.java
@@ -1,6 +1,10 @@
 package com.fzd.cloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +24,17 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    SimpleDiscoveryProperties discoveryProperties;
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping(value = "/{id}")
     public Optional<User> getUserById(@PathVariable Long id){
+        ServiceInstance serviceInstance = discoveryProperties.getLocal();
+
+        ServiceInstance instance = discoveryProperties.getLocal();
+
+        logger.info("/hello, host: " + instance.getHost() + ", service_id: " + instance.getServiceId());
         return this.userRepository.findById(id);
     }
 
