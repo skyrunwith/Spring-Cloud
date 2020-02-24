d477cabe82f1dd909f1c0ea6ebfa0a5b6e283984
skyrunwith <582547045@qq.com>
2020-02-24 18:24:42 +0800

    zuul fallback

---
+++ microservice-gateway-zuul-fallback/src/main/java/com/fzd/cloud/MicroserviceGatewayZuulFallbackApplication.java
@@ -0,0 +1,15 @@
+package com.fzd.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class MicroserviceGatewayZuulFallbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGatewayZuulFallbackApplication.class, args);
+    }
+
+}
