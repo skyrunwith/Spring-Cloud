7e70a26012b018b64119babba00bd427eb467ed8
skyrunwith <582547045@qq.com>
2020-02-24 15:24:09 +0800

    Zuul 入门

---
+++ microservice-gateway-zuul/src/main/java/com/fzd/cloud/MicroserviceGatewayZuulApplication.java
@@ -0,0 +1,17 @@
+package com.fzd.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class MicroserviceGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGatewayZuulApplication.class, args);
+    }
+
+}
